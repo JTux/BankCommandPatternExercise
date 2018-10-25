2d7450b94a8fcd6be263ff583e9d64a599b4f127
Joshua Tucker <thejltucker@gmail.com>
2018-10-24 23:58:40 -0400

    Passing tests

--- CommandPattern.Tests/TransactionTests.cs
+++ CommandPattern.Tests/TransactionTests.cs
@@ -59,5 +59,64 @@ namespace CommandPattern.Tests
             //-- Assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void ProgramUI_RevertCommand_BalanceReturnsCorrectValue()
+        {
+            //-- Arrange
+            var console = new MockConsole(new string[] { "3", "200", "6", "1", "7" });
+            var program = new ProgramUI(console);
+
+            //-- Act
+            program.Run();
+
+            var expected = 500m;
+            var actual = program.GetCurrentAccount().AccountBalance;
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ProgramUI_DepositAndHistoryCommands_HistoryShouldShowDeposit()
+        {
+            //-- Arrange
+            var console = new MockConsole(new string[] { "3", "200", "5", "7" });
+            var program = new ProgramUI(console);
+
+            //-- Act
+            program.Run();
+
+            //--Arrange
+            Assert.IsTrue(console.Output.Contains("1. Deposited $200"));
+        }
+
+        [TestMethod]
+        public void ProgramUI_WithdrawAndHistoryCommands_HistoryShouldShowWithdraw()
+        {
+            //-- Arrange
+            var console = new MockConsole(new string[] { "2", "200", "5", "7" });
+            var program = new ProgramUI(console);
+
+            //-- Act
+            program.Run();
+
+            //--Arrange
+            Assert.IsTrue(console.Output.Contains("1. Withdrew $200"));
+        }
+
+        [TestMethod]
+        public void ProgramUI_RevertAndHistoryCommands_HistoryShouldShowRevert()
+        {
+            //-- Arrange
+            var console = new MockConsole(new string[] { "2", "200", "6", "1", "5", "7" });
+            var program = new ProgramUI(console);
+
+            //-- Act
+            program.Run();
+
+            //--Arrange
+            Assert.IsTrue(console.Output.Contains("2. Reverted Transaction 1"));
+        }
     }
 }
